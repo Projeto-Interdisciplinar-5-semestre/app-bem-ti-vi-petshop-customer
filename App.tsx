2bc8ef3977d730759ebd7ecc5ec3dec2b3f6cc50
Gustavo-Simoes <145218405+Gustavo-Simoes@users.noreply.github.com>
2025-04-03 20:35:00 -0300

    telaAlimentos

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/screens/Home';
+import { Produto } from './src/screens/Home/Produto';
 
 export default function App() {
   return (
     <>
-      <Home />
+      <Produto />
       <StatusBar style="auto" />
     </>
   );
