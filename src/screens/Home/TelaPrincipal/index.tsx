5c850d1b8e4dc3e4d9a71b2416d6329d816e547d
Gustavo-Simoes <145218405+Gustavo-Simoes@users.noreply.github.com>
2025-04-29 12:50:31 -0300

    tela Home finalizada

--- src/screens/Home/TelaPrincipal/index.tsx
+++ src/screens/Home/TelaPrincipal/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity, Image } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { styles } from './style';
 
@@ -7,7 +7,6 @@ export const TelaPrincipal = () => {
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
-        
 
         <View style={styles.header}>
           <FontAwesome name="home" size={24} color="black" />
@@ -16,7 +15,6 @@ export const TelaPrincipal = () => {
 
         <Text style={styles.greeting}>Olá, Ana!</Text>
         <Text style={styles.sectionTitle}>Seus agendamentos</Text>
-
 
         <View style={styles.appointmentCard}>
           <Text style={styles.appointmentTitle}>Banho e Tosa</Text>
@@ -42,40 +40,136 @@ export const TelaPrincipal = () => {
           <Text style={styles.buttonText}>VER TODOS OS AGENDAMENTOS</Text>
         </TouchableOpacity>
 
-        <Section title="Produtos que você pode gostar" />
-        <Section title="De acordo com a sua pesquisa" />
-        <Section title="Produtos que você já comprou" />
+        <Section
+          title="Produtos que você pode gostar"
+          products={[
+            {
+              name: 'Kit Shampoo + Condicionador',
+              rating: 4.5,
+              price: 'R$ 39,99',
+              image: require('../../../assets/images/shampo 3.png'),
+            },
+            {
+              name: 'Casinha para transportes de gato e cachorro',
+              rating: 5,
+              price: 'R$ 129,99',
+              image: require('../../../assets/images/casa 1.png'),
+            },
+            {
+              name: 'Kit Brinquedos para Cães filhotes e adultos',
+              rating: 5,
+              price: 'R$ 49,99',
+              image: require('../../../assets/images/briquedo 2.png'),
+            },
+            {
+              name: 'Ração Seca Golden Formula adulta',
+              rating: 4,
+              price: 'R$ 69,99',
+              image: require('../../../assets/images/image 6.png'),
+            },
+          ]}
+        />
+
+        <Section
+          title="De acordo com a sua pesquisa"
+          products={[
+            {
+              name: 'Casinha para transportes de gato e cachorro',
+              rating: 4.7,
+              price: 'R$ 129,99',
+              image: require('../../../assets/images/casa 2.png'),
+            },
+            {
+              name: 'Casinha para transportes de gato e cachorro',
+              rating: 4.7,
+              price: 'R$ 129,49',
+              image: require('../../../assets/images/casa 2.png'),
+            },
+            {
+              name: 'Casinha para transportes de gato e cachorro',
+              rating: 4.7,
+              price: 'R$ 129,99',
+              image: require('../../../assets/images/casa 2.png'),
+            },
+            {
+              name: 'Casinha para transportes de gato e cachorro',
+              rating: 4.7,
+              price: 'R$ 129,00',
+              image: require('../../../assets/images/casa 2.png'),
+            },
+          ]}
+        />
+
+        <Section
+          title="Produtos que você já comprou"
+          products={[
+            {
+              name: 'Kit Brinquedos para Cães infantis e adultos',
+              rating: 4,
+              price: 'R$ 49,99',
+              image: require('../../../assets/images/briquedo 2.png'),
+            },
+            {
+              name: 'Kit Brinquedos para Cães infantis e adultos',
+              rating: 4,
+              price: 'R$ 49,00',
+              image: require('../../../assets/images/briquedo 2.png'),
+            },
+            {
+              name: 'Kit Brinquedos para Cães infantis e adultos',
+              rating: 4,
+              price: 'R$ 49,99',
+              image: require('../../../assets/images/briquedo 2.png'),
+            },
+            {
+              name: 'Kit Brinquedos para Cães infantis e adultos',
+              rating: 4,
+              price: 'R$ 49,90',
+              image: require('../../../assets/images/briquedo 2.png'),
+            },
+          ]}
+        />
 
       </ScrollView>
 
-    {/* Menu Inferior */}
-    <View style={styles.menu}>
-             <TouchableOpacity style={styles.menuItem}>
-               <FontAwesome name="home" size={24} />
-               <Text style={styles.menuText}>Home</Text>
-             </TouchableOpacity>
-             <TouchableOpacity style={styles.menuItem}>
-               <FontAwesome name="shopping-cart" size={24} />
-               <Text style={styles.menuText}>Comprar</Text>
-             </TouchableOpacity>
-             <TouchableOpacity style={styles.menuItem}>
-               <FontAwesome name="paw" size={24} />
-               <Text style={styles.menuText}>Serviços</Text>
-             </TouchableOpacity>
-             <TouchableOpacity style={styles.menuItem}>
-               <FontAwesome name="user" size={24} />
-               <Text style={styles.menuText}>Perfil</Text>
-             </TouchableOpacity>
-        </View>
+      {/* Menu Inferior */}
+      <View style={styles.menu}>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesome name="home" size={24} />
+          <Text style={styles.menuText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesome name="shopping-cart" size={24} />
+          <Text style={styles.menuText}>Comprar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesome name="paw" size={24} />
+          <Text style={styles.menuText}>Serviços</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesome name="user" size={24} />
+          <Text style={styles.menuText}>Perfil</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-type SectionProps = {
-  title: string;
+
+type Product = {
+  name: string;
+  rating: number;
+  price: string;
+  image: any;
 };
 
-const Section = ({ title }: SectionProps) => {
+type SectionProps = {
+  title: string;
+  products: Product[];
+};
+
+
+const Section = ({ title, products }: SectionProps) => {
   return (
     <View style={styles.section}>
       <Text style={styles.sectionTitle}>{title}</Text>
@@ -84,13 +178,25 @@ const Section = ({ title }: SectionProps) => {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.productList}
       >
-        {[1, 2, 3, 4].map((item: number, index: number) => (
+               {products.map((product, index) => (
           <View style={styles.productCard} key={index}>
-            <View style={styles.productImagePlaceholder} />
-            <Text style={styles.productName}>Produto {index + 1}</Text>
-            <Text style={styles.productPrice}>R$ 99,99</Text>
+            <Image source={product.image} style={styles.productImage} />
+            <Text style={styles.productName}>{product.name}</Text>
+            <View style={styles.ratingContainer}>
+              <Text style={styles.ratingText}>{product.rating.toFixed(1)}</Text>
+              {[...Array(5)].map((_, i) => (
+                <FontAwesome
+                  key={i}
+                  name="star"
+                  size={14}
+                  color={i < Math.round(product.rating) ? '#ffc107' : '#ccc'}
+                />
+              ))}
+            </View>
+            <Text style={styles.productPrice}>{product.price}</Text>
           </View>
         ))}
+
       </ScrollView>
       <TouchableOpacity style={styles.buttonSmall} onPress={() => {}}>
         <Text style={styles.buttonText}>VER MAIS</Text>
